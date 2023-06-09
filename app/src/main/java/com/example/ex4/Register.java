8579ddd1df5e152c8dc34f49de993f29e190ab09
Adi Ben Yehuda <adiby2017@gmail.com>
2023-06-09 19:51:14 +0300

    fix the login and register and add contacts list

---
+++ app/src/main/java/com/example/ex4/Register.java
@@ -0,0 +1,42 @@
+package com.example.ex4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    private final int GALLERY_REQ_CODE = 1000;
+    ImageView imgGallery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        imgGallery = findViewById(R.id.image);
+        Button bthGallery = findViewById(R.id.gallery);
+
+        // Show the image that the user uploads
+        bthGallery.setOnClickListener(v -> {
+            Intent iGallery = new Intent(Intent.ACTION_PICK);
+            iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(iGallery, GALLERY_REQ_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == GALLERY_REQ_CODE) {
+                imgGallery.setImageURI(data.getData());
+            }
+        }
+    }
+}
