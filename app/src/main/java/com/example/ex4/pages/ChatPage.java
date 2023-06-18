b308094d302525cada24c0d9ee980271a14a386e
ShaharMosh <shaharmosho@gmail.com>
2023-06-18 16:34:01 +0300

    get messages request

--- app/src/main/java/com/example/ex4/pages/ChatPage.java
+++ app/src/main/java/com/example/ex4/pages/ChatPage.java
@@ -13,17 +13,24 @@ import com.example.ex4.R;
 import com.example.ex4.api.ChatAPI;
 import com.example.ex4.api.ICallback;
 import com.example.ex4.schemas.Contact;
+import com.example.ex4.schemas.Message;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 public class ChatPage extends AppCompatActivity {
+
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         String contactJson = getIntent().getStringExtra("contact");
+
+        //setId(Integer.parseInt(getIntent().getStringExtra("contactId")));
 
         // Convert the JSON string back to a Contact object using Gson
         Gson gson = new Gson();
@@ -36,6 +43,8 @@ public class ChatPage extends AppCompatActivity {
         Picasso.get().load(contact.getUser().getProfilePic()).into(contactImage);
         contactName.setText(contact.getUser().getDisplayName());
 
+        setId(contact.getId());
+
         NavigateToContacts();
         getMessagesChat();
     }
@@ -44,11 +53,11 @@ public class ChatPage extends AppCompatActivity {
 
         ChatAPI chatAPI = new ChatAPI();
 
-        chatAPI.getMessages(MyApplication.getToken(), id, new ICallback() {
+        chatAPI.getMessages(MyApplication.getToken(), getId(), new ICallback() {
             @Override
             public void status(boolean status) {
                 if(status) {
-                    //finish();
+                    List<Message> messages= chatAPI.getMessages();
                 } else {
                     String error = chatAPI.getError();
                     TextView errorElement = findViewById(R.id.error);
@@ -81,6 +90,13 @@ public class ChatPage extends AppCompatActivity {
         }
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     ///////////////////////////////////////////////////////////////check
 //    @Override
